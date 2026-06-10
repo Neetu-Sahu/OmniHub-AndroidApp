257316f1570b1bd8ba77ff435e62ad6f1c3d349b
NEETU SAHU <neetusahu183@gmail.com>
2026-06-10 14:44:15 +0530

    services page updated with additional application services

--- app/src/main/java/com/example/loginregister/Ninth.java
+++ app/src/main/java/com/example/loginregister/Ninth.java
@@ -12,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Ninth extends AppCompatActivity {
-    Button b19;
+    Button b19,b33;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +20,20 @@ public class Ninth extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ninth);
         b19=(Button) findViewById(R.id.button19);
+        b33=(Button) findViewById(R.id.button33);
         b19.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(Ninth.this,Tenth.class);
                 startActivity(i);
+                finish();
+            }
+        });
+        b33.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j=new Intent(Ninth.this,Fourteenth.class);
+                startActivity(j);
                 finish();
             }
         });
